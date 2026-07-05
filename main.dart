3fd6783eba6ac7373cf25e82b94d9b124d9659d5
lucasFilppe <lucasfilipe2323@gamil.com>
2026-07-05 10:35:06 -0300

    poliformismo, herança e abstração

--- main.dart
+++ main.dart
@@ -23,7 +23,24 @@ void main() {
 
   //chamando metodo
   celular1.caracteristicasCelular();
+
+   //orientação a objetos
+  Carro porsche = Carro("panamera", 701116338636);
+  print(porsche.modelo);
+  print(porsche._cpfComprador);
+
+  //herança, poliformismo e abstração
+  PessoaLucas lucas = PessoaLucas();
+  PessoaMaria maria = PessoaMaria();
+
+  print(lucas.comunicar());
+  print(maria.comunicar());
+
+   Cachorro dog = Cachorro();
+  dog.dormir(); // Herdado da classe Animal
+  dog.latir();  // Método próprio
 }
+
 
 //quando criarmos classes lembre de uma analogia, ela é um objeto do mundo real
 class Celular {
@@ -39,4 +56,73 @@ class Celular {
   void caracteristicasCelular() {
     print('Nome: $nomeDispositivo , RS = $preco, Marca: $marca');
   }
+
+ 
 }
+
+//progmação orientada a objetos = classes e objetos
+//conceito fundamentais
+class Carro{
+  final String modelo;
+
+  //ecapsulamento
+  int _cpfComprador;
+
+  Carro(this.modelo, this._cpfComprador);
+
+   int get cpfComprador => _cpfComprador;
+
+  set cpfComprador(int novoCpf) {
+    _cpfComprador = novoCpf;
+  }
+
+}
+
+//herança, poliformismo e abstração
+//a abstração é o como um contarato que qaulquer pessoa deve seguir
+//Uma classe abstrata define um contrato. Ela diz o que as classes devem fazer, mas não como.
+abstract class Pessoa{
+  String comunicar();// a pessoa deve se comunicar
+}
+
+//Qualquer classe que implemente Pessoa é obrigada a criar o método comunicar().
+class PessoaLucas implements Pessoa {
+  @override
+  String comunicar() {
+    return "Lucas gosta de comunicar falando de Deus.";
+  }
+}
+
+class PessoaMaria implements Pessoa {
+  @override
+  String comunicar() {
+    return "Maria gosta de comunicar cantando.";
+  }
+}
+
+//Herança é quando uma classe herda características de outra.
+class Animal {
+  void dormir() {
+    print("O animal está dormindo.");
+  }
+}
+
+class Cachorro extends Animal {
+  void latir() {
+    print("Au Au!");
+  }
+}
+/**
+ * Resumo
+Conceito	Palavra-chave	Exemplo
+Abstração	abstract	Define um contrato que outras classes devem seguir.
+Herança	extends	Uma classe herda atributos e métodos de outra.
+Polimorfismo	@override	O mesmo método tem comportamentos diferentes em
+ cada classe.
+Interface	implements	A classe é obrigada a implementar todos os 
+métodos da interface/classe abstrata.
+Observação: no seu exemplo você usou implements. Em Dart, é mais comum usar
+ extends quando a classe abstrata fornece comportamento compartilhado, e 
+ implements quando você quer apenas obrigar a implementação de todos os
+  métodos, como uma interface.
+ */
