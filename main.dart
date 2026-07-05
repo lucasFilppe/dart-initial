8d3deea2449674d77fe156ac15b50c98d23a89bb
lucasFilppe <lucasfilipe2323@gamil.com>
2026-07-05 00:27:57 -0300

    atualizando curdo dart

---
+++ main.dart
@@ -0,0 +1,21 @@
+void main(){
+  // 1- introdução a variaveis
+  String nome = "Lucas";
+  print(nome);
+  int idade = 27;
+  print(idade);
+  bool DeusEhFiel = true;
+  print(DeusEhFiel);
+
+  List<String> tecnologias = ["flutter", "next", "react"];
+  print(tecnologias);
+  print('tecnologia que estou aprendendo no momento ${tecnologias[0]}');
+
+  // 2 - null-safety
+  String? time;//quando colocams uma interrogação apos o tipo, 
+  //estamos indicando que o valor pode ser null
+  print(time);
+
+  //metodos e classes
+  
+}
